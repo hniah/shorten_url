7d66862ce4663929c5ec571821b8a105876ac5a4
Anthony <anthony.hoang@simplelabx.io>
2022-07-07 05:57:06 +0700

    Rspec: encode api

---
+++ spec/controllers/api/shorteners_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe Api::ShortenersController, type: :controller do
+  before do
+    request.headers['Content-Type'] = 'application/json'
+  end
+
+  describe '#encode' do
+    
+    def do_request(url)
+      post :encode, params: { url: url }
+    end
+
+    context 'With a valid url' do
+      let(:original_url) { 'https://guides.rubyonrails.org/active_record_basics.html#the-active-record-pattern' }
+      it 'is success' do
+        do_request(original_url)
+        result = JSON.parse(response.body).with_indifferent_access
+        expect(result[:error_code]).to eq 0
+        expect(result[:data][:original_url]).to eq original_url
+        expect(result[:data][:short_url]).not_to be be_empty
+      end
+    end
+
+    context 'With a invalid url' do
+      it 'is failure' do
+        do_request('invalid_url')
+        result = JSON.parse(response.body).with_indifferent_access
+        expect(result[:error_code]).not_to be 0
+      end
+    end
+  end
+end
