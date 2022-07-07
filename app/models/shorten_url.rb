56d4954c9082f003c71142cc79a3cdb06e8436c8
Anthony <anthony.hoang@simplelabx.io>
2022-07-07 19:28:55 +0700

    Fix duplicate slug

--- app/models/shorten_url.rb
+++ app/models/shorten_url.rb
@@ -2,11 +2,12 @@
 
 class ShortenUrl < ApplicationRecord
   ALPHA_NUMBER = [*('a'..'z'), *('A'..'Z'), *('0'..'9')].freeze
+  SLUG_LENGTH = 6.freeze
 
   validates :original_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
-  validates :slug, presence: true, uniqueness: true
+  validates_length_of :original_url, maximum: 500, on: :create, message: "url too long"
 
-  before_validation :generate_slug
+  before_create :generate_slug
 
   def short_url
     Rails.application.routes.url_helpers.short_url(slug: slug)
@@ -15,6 +16,13 @@ class ShortenUrl < ApplicationRecord
   private
 
   def generate_slug
-    self.slug = ALPHA_NUMBER.sample(6).join if slug.blank?
+    self.slug = ALPHA_NUMBER.sample(SLUG_LENGTH).join
+    generate_slug if slug_existed?
+  end
+
+  def slug_existed?
+    self.class.where(slug: self.slug).exists?
   end
 end
+
+
