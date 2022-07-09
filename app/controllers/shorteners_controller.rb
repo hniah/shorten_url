a6264f9b875d1bb06fafbd472ff9dd7b1ec2f91a
Anthony <anthony.hoang@simplelabx.io>
2022-07-10 05:59:51 +0700

    Setup home screen

--- app/controllers/shorteners_controller.rb
+++ app/controllers/shorteners_controller.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 class ShortenersController < ApplicationController
-  def show
-    shorten_url = ShortenUrl.find_by(slug: params[:slug])
-    raise ActionController::RoutingError, 'Not Found' if shorten_url.nil?
+  def home; end
 
-    redirect_to shorten_url.original_url
+  def show
+    shorten_url = ShortenUrl.get_by_slug(slug)
+    raise_not_found if shorten_url.nil?
+    redirect_to shorten_url.original_url, status: 301
+  end
+
+  private
+
+  def slug
+    params[:slug]
+  end
+
+  def raise_not_found
+    raise ActionController::RoutingError.new('Not Found')
   end
 end
